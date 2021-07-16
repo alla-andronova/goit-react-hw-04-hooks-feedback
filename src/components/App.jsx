75f03f6f33be29cd90d65b461660625fc9084f30
alla-andronova <allaitcreative@gmail.com>
2021-07-16 19:17:14 +0300

    refactor to hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,60 +1,64 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Statistics from './statistics/Statistics';
 import FeedbackOptions from './feedbackOptions/FeedbackOptions';
 
-export default class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
+export default function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  handleOnClick = e => {
+  const feedback = {
+    good,
+    neutral,
+    bad,
+  };
+  const totalFeedback = good + neutral + bad;
+
+  const handleOnClick = e => {
     const { name } = e.target;
+    switch (name) {
+      case 'good':
+        setGood(state => state + 1);
+        break;
+      case 'neutral':
+        setNeutral(state => state + 1);
+        break;
+      case 'bad':
+        setBad(state => state + 1);
+        break;
 
-    this.setState(prevState => ({
-      [name]: prevState[name] + 1,
-    }));
+      default:
+        return;
+    }
   };
 
-  countTotalFeedback = () => {
-    return Object.values(this.state).reduce((acc, value) => acc + value, 0);
-  };
-
-  countPositiveFeedbackPercentage = () => {
-    const totalFeedback = this.countTotalFeedback();
+  const countPositiveFeedbackPercentage = () => {
     if (totalFeedback === 0) {
       return 0;
     }
-    return Math.round((this.state.good / totalFeedback) * 100);
+    return Math.round((good / totalFeedback) * 100);
   };
 
-  render() {
-    const totalFeedback = this.countTotalFeedback();
-    return (
-      <>
-        <FeedbackOptions
-          feedbacks={this.state}
-          handleOnClick={this.handleOnClick}
-        />
+  return (
+    <>
+      <FeedbackOptions feedbacks={feedback} handleOnClick={handleOnClick} />
 
-        {totalFeedback > 0 ? (
-          <Statistics
-            feedbacks={this.state}
-            total={this.countTotalFeedback()}
-            positivePercentage={this.countPositiveFeedbackPercentage()}
-          />
-        ) : (
-          <p
-            style={{
-              textAlign: 'center',
-              fontSize: 'larger',
-            }}
-          >
-            No feedback given.
-          </p>
-        )}
-      </>
-    );
-  }
+      {totalFeedback > 0 ? (
+        <Statistics
+          feedbacks={feedback}
+          total={totalFeedback}
+          positivePercentage={countPositiveFeedbackPercentage()}
+        />
+      ) : (
+        <p
+          style={{
+            textAlign: 'center',
+            fontSize: 'larger',
+          }}
+        >
+          No feedback given.
+        </p>
+      )}
+    </>
+  );
 }
